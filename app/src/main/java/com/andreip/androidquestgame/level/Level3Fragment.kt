7b714bdb8ff6fc63ffc15d0ce045b07b0950343f
Andrei_Papko <andrei_papko@epam.com>
2018-11-14 13:15:44 +0300

    cola level removed

--- app/src/main/java/com/andreip/androidquestgame/level/Level3Fragment.kt
+++ app/src/main/java/com/andreip/androidquestgame/level/Level3Fragment.kt
@@ -7,5 +7,5 @@ import com.andreip.androidquestgame.state.QuestState
 class Level3Fragment : BaseLevelFragment() {
     override val questState: QuestState = QuestState.LEVEL_3
     override val levelLayoutId: Int = R.layout.fragment_level_3
-    override val navigateActionId: Int = R.id.action_level3Fragment_to_level4Fragment
+    override val navigateActionId: Int = R.id.action_level3Fragment_to_level404Fragment
 }
